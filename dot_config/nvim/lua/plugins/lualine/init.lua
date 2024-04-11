f35c9ec901a19a697fa04e65c5a3937672d9837e
Xavier Sanna <22643870+xavier-sanna@users.noreply.github.com>
2024-04-12 01:19:37 +0200

    [nvim] disable lualine in neotree

--- dot_config/nvim/lua/plugins/lualine/init.lua
+++ dot_config/nvim/lua/plugins/lualine/init.lua
@@ -20,11 +20,10 @@ end
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  event = "VeryLazy",
   opts = {
     options = {
       disabled_filetypes = {
-    --    'neo-tree'
+        'neo-tree'
       },
     },
     extensions = {'neo-tree'}
