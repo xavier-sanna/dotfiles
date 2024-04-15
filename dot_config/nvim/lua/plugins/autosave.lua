fbd611999337617128ad3b522c0878007b5ca86a
Xavier Sanna <22643870+xavier-sanna@users.noreply.github.com>
2024-04-15 12:12:03 +0200

    [nvim] add autosave and spectre plugins

---
+++ dot_config/nvim/lua/plugins/autosave.lua
@@ -0,0 +1,48 @@
+return {
+  "okuuva/auto-save.nvim",
+  cmd = "ASToggle", -- optional for lazy loading on command
+  event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+
+  opts = {
+    enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
+    execution_message = {
+      enabled = true,
+      message = function() -- message to print on save
+        return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+      end,
+      dim = 0.18, -- dim the color of `message`
+      cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+    },
+    trigger_events = { -- See :h events
+      immediate_save = { "BufLeave", "FocusLost" }, -- vim events that trigger an immediate save
+      defer_save = { "InsertLeave", "TextChanged" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
+      cancel_defered_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
+    },
+    -- function that takes the buffer handle and determines whether to save the current buffer or not
+    -- return true: if buffer is ok to be saved
+    -- return false: if it's not ok to be saved
+    -- if set to `nil` then no specific condition is applied
+    condition = nil,
+    write_all_buffers = false, -- write all buffers when the current one meets `condition`
+    noautocmd = false, -- do not execute autocmds when saving
+    debounce_delay = 1000, -- delay after which a pending save is executed
+   -- log debug messages to 'auto-save.log' file in neovim cache directory, set to `true` to enable
+    debug = false,
+  },
+  config = function(_, opts)
+    require('auto-save').setup(opts)
+
+    local group = vim.api.nvim_create_augroup('autosave', {})
+
+    vim.api.nvim_create_autocmd('User', {
+        pattern = 'AutoSaveWritePost',
+        group = group,
+        callback = function(opts)
+            if opts.data.saved_buffer ~= nil then
+                local filename = vim.api.nvim_buf_get_name(opts.data.saved_buffer)
+                print('We have saved ' .. filename .. ' get ready captain!')
+            end
+        end,
+    })
+  end
+}
