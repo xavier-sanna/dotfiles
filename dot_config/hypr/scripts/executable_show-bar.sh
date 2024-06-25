0b151df68f3d4c6c2ca7bd7843570c22a6cf2eed
Xavier Sanna <22643870+xavier-sanna@users.noreply.github.com>
2024-06-25 03:48:14 +0200

    add descriptions to binds with hyprland

--- dot_config/hypr/scripts/executable_show-bar.sh
+++ dot_config/hypr/scripts/executable_show-bar.sh
@@ -2,10 +2,6 @@
 
 check_active=$(~/.local/bin/eww active-windows | grep 'topbar')
 
-EXCLUSIVE="${1:-false}"
-
-echo "$EXCLUSIVE" >>~/testlog
-
 if [[ -n $check_active ]]; then
 	exit 0
 fi
@@ -14,7 +10,6 @@ fi
 	topbar:tb_primary \
 	topbar:tb_second \
 	topbar:tb_third \
-	--arg tb_primary:monitor=0 \
-	--arg tb_second:monitor=1 \
-	--arg tb_third:monitor=2 \
-	--arg excl="$EXCLUSIVE"
+	--arg tb_primary:monitor="0" \
+	--arg tb_second:monitor="1" \
+	--arg tb_third:monitor="2"
