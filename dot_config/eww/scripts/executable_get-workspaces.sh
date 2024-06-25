26a45984e3fe7d5b256481cb878a93b7b16392e9
Xavier Sanna <22643870+xavier-sanna@users.noreply.github.com>
2024-06-25 03:47:32 +0200

    update eww workspaces script with autonamed clients

--- dot_config/eww/scripts/executable_get-workspaces.sh
+++ dot_config/eww/scripts/executable_get-workspaces.sh
@@ -1,24 +1,108 @@
-#!/usr/bin/env bash
-
-# spaces() {
-# 	WORKSPACE_WINDOWS="$(hyprctl workspaces -j | jq --arg monitor "$MONITOR" 'map(select(.monitor == $monitor)) | map({key: .id | tostring, value: .windows}) | from_entries')"
-# 	seq 1 10 | jq --argjson windows "${WORKSPACE_WINDOWS}" --slurp -Mc 'map(tostring) | map({id: ., windows: ($windows[.]//0)})'
-# }
-#
-# spaces
-# socat -u UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do
-# 	spaces
-# done
-#
-#
+#!/bin/bash
 
 workspaces() {
-	workspaces_rules=$(hyprctl workspacerules -j | jq -r ".[] | {name: .workspaceString, monitor: .monitor, empty: true}")
-	workspaces_open=$(hyprctl workspaces -j | jq -r ".[] | {name: .name, monitor: .monitor, empty: false}")
+	workspaces_json=$(hyprctl workspaces -j)
+	workspacerules_json=$(hyprctl workspacerules -j)
+	monitors_json=$(hyprctl monitors -j)
+	binds_json=$(hyprctl binds -j)
+	clients_json=$(hyprctl clients -j)
+	clients_config_json=$(jq -r '.' ~/.config/eww/scripts/clients-config.json)
 
-	echo "$workspaces_rules$workspaces_open" |
-		jq -n '[inputs]' |
-		jq 'reduce .[] as $o ({}; .[$o.monitor] += reduce $o as $item ({}; .[$item.name] = {empty:$item.empty}))'
+	# Build the clients class/title => icon map
+	clients_map=$(jq --argjson config "$clients_config_json" '
+	 reduce .[] as $item (
+	   {}; .[$item.workspace.id|tostring] += [
+	       if $config[$item.class] | type == "object"
+	       then
+           (
+             $config[$item.class].extra[] as {$title, $name} |
+             if 
+               ($item.title | test($title)) 
+             then 
+               $name 
+             else 
+               empty 
+             end
+           ) // $config[$item.class].defaultName
+	       else
+	          $config[$item.class]
+	       end
+	     ]
+	 )' <<<"$clients_json")
+
+	# Create the initial structure from monitors
+	initial_structure=$(jq '
+  map({
+    key: (.id | tostring),
+    value: {
+      name: .name,
+      workspaces: []
+    }
+  }) | from_entries' <<<"$monitors_json")
+
+	# Create a mapping of monitor names to IDs
+	monitor_map=$(jq '
+  map({
+    (.name): {id: .id, activeWorkspaceId: .activeWorkspace.id},
+  }) | add' <<<"$monitors_json")
+
+	# Populate workspaces from rules
+	workspaces_populated=$(jq --argjson initial "$initial_structure" --argjson monitor_map "$monitor_map" --argjson clients "$clients_map" '
+  reduce .[] as $item ($initial;
+    .[$monitor_map[$item.monitor].id | tostring].workspaces += [{
+      id: ($item.workspaceString | tonumber),
+      name: $item.workspaceString,
+      active: false,
+      empty: true,
+      clients: $clients[$item.workspaceString]
+    }]
+  )' <<<"$workspacerules_json")
+
+	# Update states with monitor maps and busy workspaces
+	workspaces_stateful=$(jq --argjson ws_populated "$workspaces_populated" --argjson monitor_map "$monitor_map" '
+  reduce .[] as $item ($ws_populated;
+    .[$item.monitorID | tostring].workspaces |= map(
+      if (.id | tostring) == ($item.id | tostring) then
+        . + {
+          active: ($monitor_map[$item.monitor | tostring].activeWorkspaceId == $item.id),
+          empty: false
+        }
+      else
+        .
+      end
+    )
+  )' <<<"$workspaces_json")
+
+	# Build a map of workspace binds with their key
+	workspace_keys_map=$(jq '
+	     map(select(.dispatcher == "workspace")) |
+	     map(select(.description|test("select workspace:"))) |
+	     map({workspaceID: .arg, key: .key})
+	   ' <<<"$binds_json")
+
+	# Update workspace names with keys from the additional dataset
+	workspaces_keys=$(jq --argjson stateful "$workspaces_stateful" --argjson keys "$workspace_keys_map" '
+  . as $in
+  | $keys as $keysData
+  | $keysData
+  | reduce .[] as $keyItem ($stateful; 
+      . as $wsStateful 
+      | reduce ($wsStateful | to_entries)[] as $monEntry (
+          $wsStateful;
+          .[$monEntry.key].workspaces |= map(
+              if .id == ($keyItem.workspaceID | tonumber) then
+                  .name = $keyItem.key
+              else
+                  .
+              end
+          )
+      )
+  )' <<<"$workspaces_stateful")
+
+	echo $workspaces_keys
 }
 
 workspaces
+socat -u UNIX-CONNECT:"$XDG_RUNTIME_DIR"/hypr/"$HYPRLAND_INSTANCE_SIGNATURE"/.socket2.sock - | while read -r; do
+	workspaces
+done
