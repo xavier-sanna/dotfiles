49eb291d421cef9a1d34719674db3fa4fe0b8bf4
Xavier Sanna <22643870+xavier-sanna@users.noreply.github.com>
2024-07-05 23:52:09 +0200

    [eww] add clock widget + update classes

--- dot_config/hypr/scripts/executable_show-powermenu.sh
+++ dot_config/hypr/scripts/executable_show-powermenu.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/bash
 
-check_active=$(~/.local/bin/eww active-windows | grep 'powermenu_toggle_workspace')
+check_active=$(~/.local/bin/eww active-windows | grep 'powermenu_toggle')
 
 if [[ -n $check_active ]]; then
 	exit 0
 fi
 
 ~/.local/bin/eww open-many \
-	powermenu_toggle_workspace:pmw_primary \
-	powermenu_toggle_workspace:pmw_second \
-	powermenu_toggle_workspace:pmw_third \
+	powermenu_toggle:pmw_primary \
+	powermenu_toggle:pmw_second \
+	powermenu_toggle:pmw_third \
 	--arg pmw_primary:monitor="0" \
 	--arg pmw_second:monitor="1" \
 	--arg pmw_third:monitor="2"
