c8191c498c12d7b398c7e60aee8432fae2067093
Xavier Sanna <22643870+xavier-sanna@users.noreply.github.com>
2026-02-10 14:03:37 +0100

    update ansiblels conf

--- dot_config/nvim/lua/plugins/lsp/settings/ansiblels.lua
+++ dot_config/nvim/lua/plugins/lsp/settings/ansiblels.lua
@@ -1,21 +1,36 @@
 local util = require("lspconfig.util")
 
 return {
-	root_dir = function(pattern)
-		local cwd = vim.uv.cwd()
-		local root = util.root_pattern("ansible.cfg")(pattern)
-
-		-- prefer cwd if root is a descendant
-		return util.path.is_descendant(cwd, root) and cwd or root
-	end,
-	root_markers = { "ansible.cfg", "ansible-lint" },
+	-- root_dir = function(pattern)
+	-- 	local cwd = vim.uv.cwd()
+	-- 	local root = util.root_pattern("ansible.cfg", ".ansible-lint")(pattern)
+	--
+	-- 	-- prefer cwd if root is a descendant
+	-- 	if root and util.path.is_descendant(cwd, root) then
+	-- 		return cwd
+	-- 	end
+	--
+	-- 	return root
+	-- end,
+	root_markers = { "ansible.cfg", ".ansible-lint" },
 	filetypes = { "yaml.ansible" },
 	settings = {
-		validation = {
-			enabled = true,
-			lint = {
+		ansible = {
+			ansible = {
+				path = "ansible",
+			},
+			executionEnvironment = {
+				enabled = false,
+			},
+			python = {
+				interpreterPath = "python",
+			},
+			validation = {
 				enabled = true,
-				path = "ansible-lint",
+				lint = {
+					enabled = true,
+					path = "ansible-lint",
+				},
 			},
 		},
 	},
