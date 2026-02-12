8b3b77bbf1a6de06a31ca4fe23ee528e01cd0352
Xavier Sanna <22643870+xavier-sanna@users.noreply.github.com>
2026-02-12 23:20:10 +0100

    update intelephense conf

--- dot_config/nvim/lua/plugins/lsp/settings/intelephense.lua
+++ dot_config/nvim/lua/plugins/lsp/settings/intelephense.lua
@@ -1,13 +1,9 @@
 local util = require("lspconfig.util")
 
 return {
-	root_dir = function(pattern)
-		local cwd = vim.uv.cwd()
-		local root = util.root_pattern("composer.json")(pattern)
-
-		-- prefer cwd if root is a descendant
-		return util.path.is_descendant(cwd, root) and cwd or root
-	end,
+	cmd = { "intelephense", "--stdio" },
+	filetypes = { "php" },
+	root_markers = { ".git", "composer.json" },
 	init_options = {
 		licenceKey = vim.fn.expand("$HOME/.intelephense/license.txt"),
 		storagePath = vim.fn.expand("$HOME/.intelephense"),
